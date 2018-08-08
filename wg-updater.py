757b9a5bdb5172b2793db51acd347fd3c7249178
DerGut <jannik.steinmann@gmx.de>
2018-08-08 17:52:02 +0200

    Add requirements file

--- wg-updater.py
+++ wg-updater.py
@@ -7,7 +7,6 @@ from configparser import ConfigParser
 from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
 
 # Load configuration
 config = ConfigParser()

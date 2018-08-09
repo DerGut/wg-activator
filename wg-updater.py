bab7296e5a0f013bdc3dc840a5aa8af436d7f435
DerGut <jannik.steinmann@gmx.de>
2018-08-09 16:59:17 +0200

    Imprive error logging

--- wg-updater.py
+++ wg-updater.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
 import logging
 import random
-
 import time
 from configparser import ConfigParser
 
-from datetime import datetime
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException, WebDriverException
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 
 logging.basicConfig(
     filename='updates.log',
     filemode='a',
-    level=logging.INFO
+    level=logging.INFO,
+    format='%(levelname)s::%(asctime)-15s::%(message)s'
 )
 
 # Load configuration
@@ -26,6 +26,8 @@ LISTING_URL = config.get('Listing', 'listing_url')
 DELAY = int(config.get('Driver', 'delay'))
 ENV = config.get('System', 'environment')
 MARGIN = config.get('Driver', 'margin')
+
+logging.info('Starting wg-updater')
 
 # Login
 options = Options()
@@ -46,28 +48,34 @@ driver.find_element_by_id('login_submit').click()
 time.sleep(5)
 
 while True:
-    # Open listing
-    driver.get(
-        LISTING_URL)
-    time.sleep(3)
-    contact = driver.find_element_by_class_name('bottom_contact_box')
-    edit = contact.find_element_by_link_text('ANGEBOT BEARBEITEN')
-    edit.send_keys(Keys.TAB)
-    time.sleep(1)
-    edit.click()
-    time.sleep(3)
+    try:
+        # Open listing
+        driver.get(
+            LISTING_URL)
+        time.sleep(3)
+        contact = driver.find_element_by_class_name('bottom_contact_box')
+        edit = contact.find_element_by_link_text('ANGEBOT BEARBEITEN')
+        edit.send_keys(Keys.TAB)
+        time.sleep(1)
+        edit.click()
+        time.sleep(3)
 
-    # Refresh listing
-    btn = driver.find_element_by_class_name('btn-orange') # weiter
-    btn.send_keys(Keys.TAB)
-    time.sleep(2)
-    btn.send_keys(Keys.SPACE)
-    btn = driver.find_element_by_class_name('btn-orange') # Änderungen ubernehmen
-    btn.send_keys(Keys.TAB)
-    time.sleep(2)
-    btn.send_keys(Keys.SPACE)
-    assert 'zehn Minuten' in driver.page_source
-    logging.info("Reloaded at {}".format(datetime.now().strftime("%Y-%m-%d %H:%M")))
+        # Refresh listing
+        btn = driver.find_element_by_class_name('btn-orange') # weiter
+        btn.send_keys(Keys.TAB)
+        time.sleep(2)
+        btn.send_keys(Keys.SPACE)
+        btn = driver.find_element_by_class_name('btn-orange') # Änderungen ubernehmen
+        btn.send_keys(Keys.TAB)
+        time.sleep(2)
+        btn.send_keys(Keys.SPACE)
+        assert 'zehn Minuten' in driver.page_source
+        logging.info("Reloaded")
+
+    except NoSuchElementException as e:
+        logging.error(f"{e}\nTry to increase the waiting time if the page wasn't fully loaded")
+    except WebDriverException as e:
+        logging.error(e)
 
     # offset = percent of margin around delay
     if MARGIN and MARGIN.isdigit() and MARGIN != 0.0:

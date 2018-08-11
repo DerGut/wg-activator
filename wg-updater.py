7edb1b873198cfb2f670892f69ac022c3340555b
DerGut <jannik.steinmann@gmx.de>
2018-08-12 01:56:06 +0200

    Some refactoring + driver reopening on failure

--- wg-updater.py
+++ wg-updater.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 import logging
 import random
 import time
@@ -13,78 +12,89 @@ logging.basicConfig(
     filename='updates.log',
     filemode='a',
     level=logging.INFO,
-    format='%(levelname)s::%(asctime)-15s::%(message)s'
+    format='%(levelname)s:%(asctime)-15s:%(message)s'
 )
 
-# Load configuration
-config = ConfigParser()
-conf_file = open('config.ini')
-config.readfp(conf_file)
-USERNAME = config.get('Login', 'email')
-PASSWORD = config.get('Login', 'password')
-LISTING_URL = config.get('Listing', 'listing_url')
-DELAY = int(config.get('Driver', 'delay'))
-ENV = config.get('System', 'environment')
-MARGIN = float(config.get('Driver', 'margin')) or 0.0
 
-logging.info('Starting up...')
+def login(options):
+    driver = webdriver.Chrome(options=options)
+    driver.get('https://wg-gesucht.de')
 
-# Login
-options = Options()
-if ENV == 'production':
-    options.add_argument('--headless')
-driver = webdriver.Chrome(options=options)
-driver.get('https://wg-gesucht.de')
+    # Remove cookie button
+    driver.find_element_by_id("cookie-confirm").click()
+    driver.find_element_by_link_text("LOGIN").click()
+    time.sleep(1)
+    username = driver.find_element_by_name("login_email_username")
+    password = driver.find_element_by_id("login_password")
+    username.send_keys(USERNAME)
+    password.send_keys(PASSWORD)
+    driver.find_element_by_id('login_submit').click()
+    time.sleep(5)
 
-# Remove cookie button
-btn = driver.find_element_by_id("cookie-confirm").click()
-driver.find_element_by_link_text("LOGIN").click()
-time.sleep(1)
-username = driver.find_element_by_name("login_email_username")
-password = driver.find_element_by_id("login_password")
-username.send_keys(USERNAME)
-password.send_keys(PASSWORD)
-driver.find_element_by_id('login_submit').click()
-time.sleep(5)
+    return driver
 
-while True:
-    try:
-        logging.info('Starting to reload')
 
-        # Open listing
-        driver.get(LISTING_URL)
-        time.sleep(3)
-        contact = driver.find_element_by_class_name('bottom_contact_box')
-        edit = contact.find_element_by_link_text('ANGEBOT BEARBEITEN')
-        edit.send_keys(Keys.TAB)
-        time.sleep(1)
-        edit.click()
-        time.sleep(3)
+def reload(driver):
+    # Open listing
+    driver.get(LISTING_URL)
+    time.sleep(3)
+    contact = driver.find_element_by_class_name('bottom_contact_box')
+    edit = contact.find_element_by_link_text('ANGEBOT BEARBEITEN')
+    edit.send_keys(Keys.TAB)
+    time.sleep(1)
+    edit.click()
+    time.sleep(3)
 
-        # Refresh listing
-        btn = driver.find_element_by_class_name('btn-orange') # weiter
-        btn.send_keys(Keys.TAB)
-        time.sleep(2)
-        btn.send_keys(Keys.SPACE)
-        btn = driver.find_element_by_class_name('btn-orange') # Änderungen ubernehmen
-        btn.send_keys(Keys.TAB)
-        time.sleep(2)
-        btn.send_keys(Keys.SPACE)
+    # Refresh listing
+    btn = driver.find_element_by_class_name('btn-orange')  # weiter
+    btn.send_keys(Keys.TAB)
+    time.sleep(2)
+    btn.send_keys(Keys.SPACE)
+    btn = driver.find_element_by_class_name('btn-orange')  # Änderungen ubernehmen
+    btn.send_keys(Keys.TAB)
+    time.sleep(2)
+    btn.send_keys(Keys.SPACE)
 
-        assert 'zehn Minuten' in driver.page_source
+    assert 'zehn Minuten' in driver.page_source
 
-        logging.info("Reloaded")
 
-    except NoSuchElementException as e:
-        logging.error(f"{e}\nTry to increase the waiting time if the page wasn't fully loaded")
-    except WebDriverException as e:
-        logging.error(e)
+if __name__ == '__main__':
+    # Load configuration
+    config = ConfigParser()
+    conf_file = open('config.ini')
+    config.readfp(conf_file)
 
-    # offset = percent of margin around delay
-    if MARGIN != 0.0:
-        offset = int(DELAY * ((random.random() * MARGIN * 2.0) - MARGIN))
-    else:
-        offset = 0.0
+    USERNAME = config.get('Login', 'email')
+    PASSWORD = config.get('Login', 'password')
+    LISTING_URL = config.get('Listing', 'listing_url')
+    DELAY = int(config.get('Driver', 'delay'))
+    ENV = config.get('System', 'environment')
+    MARGIN = float(config.get('Driver', 'margin')) or 0.0
 
-    logging.info(f"Sleeping for {(DELAY + offset) // 60}min")
-    time.sleep(DELAY + offset)
+    driver_options = Options()
+    if ENV == 'production':
+        driver_options.add_argument('--headless')
+
+    logging.info('Starting up...')
+    web_driver = login(driver_options)
+
+    while True:
+        try:
+            logging.info('Starting to reload')
+            reload(web_driver)
+            logging.info("Reloaded")
+        except NoSuchElementException as e:
+            logging.error(f"{e}\nTry to increase the waiting time if the page wasn't fully loaded")
+        except WebDriverException as e:
+            logging.error(e)
+            web_driver.quit()
+            web_driver = login(driver_options)
+
+        # offset = percent of margin around delay
+        if MARGIN != 0.0:
+            offset = int(DELAY * ((random.random() * MARGIN * 2.0) - MARGIN))
+        else:
+            offset = 0.0
+
+        logging.info(f"Sleeping for {(DELAY + offset) // 60}min")
+        time.sleep(DELAY + offset)
